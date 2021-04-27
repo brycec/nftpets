4dcd9847eb402ae4a68ce494c9f8a65e08b9971e
bryce <hibryce@gmail.com>
2021-04-27 12:56:49 -0500

    forgot

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-   @user = User.create(params.require(:user).permit(:username,
+   @user = User.create(params.require(:user).permit(:name,
     :password))
    session[:user_id] = @user.id
    redirect_to '/welcome'
