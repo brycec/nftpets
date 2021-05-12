561b38fa4303af39c29dface78722ec476a3d465
bryce <hibryce@gmail.com>
2021-05-12 12:28:55 -0500

    index user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :name, format: { with: /\A[_a-zA-Z0-9]+\z/,
+    message: "User name can only be: _, a-z, A-Z, or 0-9"}
+  validates :name, uniqueness: true
   has_secure_password
 end
