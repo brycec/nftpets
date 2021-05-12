1cea456767ca90028c321362af2b941a0883f720
bryce <hibryce@gmail.com>
2021-05-12 00:35:25 -0500

    wow send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,13 +4,25 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.create(params.require(:message).permit(
+      :to,:subject,:body))
+    @message.from = current_user.name
+    @message.save
+    redirect_to '/messages'
   end
 
+ def my_messages
+   Message.where(to: current_user.name)
+end
   def show
+    @messages = my_messages
+    @fmessage = Message.find(params[:id])
+    render  "index"
   end
 
   def index
-    @messages = Message.all
+    @messages = my_messages
+    @fmessage = @messages.first
   end
 
   def delete
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_032846) do
+ActiveRecord::Schema.define(version: 2021_05_12_035929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2021_05_12_032846) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "subject"
     t.string "body"
+    t.string "to"
+    t.string "from"
   end
 
   create_table "tokens", force: :cascade do |t|
