f619cf3b8a99f4c4a364da3bfc45731147f9ae10
bryce <hibryce@gmail.com>
2021-05-15 18:40:22 -0500

    work work

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,25 +12,33 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(params.require(:message).permit(
-      :to,:subject,:body))
+      :to,:subject,:body,:token_id))
     m = /\A(The [A-Z][a-z]+) sent: (.+)\z/.match @message.subject
     if m
       @message.from = m[1]
       @message.subject = m[2]
     else
       @message.from = current_user.name
+      token = Token.where(id: @message.token_id).first
+      if token then
+        token.user_id = 1
+        token.save
+      end
     end
     @message.save
     redirect_to '/messages'
   end
 
   def show
+    @fmessage = Message.find(params[:id])
     if params[:cult]
       @messages = Message.where(to: "The "+params[:cult])
     else
       @messages = my_messages
+      if @fmessage.to != current_user.name
+        @fmessage = @messages.first
+      end
     end
-    @fmessage = Message.find(params[:id])
     render  "index"
   end
 
