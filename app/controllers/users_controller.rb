7beff7ceac17eef60d1077c539a7983887475c49
bryce <hibryce@gmail.com>
2021-05-22 12:50:49 -0500

    its just better ok?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all.sort {|a|-a.networth}
+    @users = User.all.sort {|a,b|b.networth<=>a.networth}
   end
 
   def show
